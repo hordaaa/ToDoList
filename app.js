1b9e268e1120483c7f1d35811db96c41e845cd9b
hordaaa <144600019+hordaaa@users.noreply.github.com>
2024-06-05 16:20:27 +0700

    comments and refactor

--- app.js
+++ app.js
@@ -3,28 +3,24 @@ let addTaskButton = document.getElementById("add-task")
 let listOfTasks = document.getElementById("list-tasks")
 let noTasks = document.getElementById("no-tasks")
 let elemetsOfTaskList = document.getElementsByClassName("task")
-let clearbutton = document.getElementById("clear-button")
+let clearButton = document.getElementById("clear-button")
 
-if (!localStorage.getItem("tasks"))
+if (!localStorage.getItem("tasks")) // Инициализируем список задач в localStorage
     localStorage.setItem("tasks", "[]")
-
 
 let arrayOftasks = JSON.parse(localStorage.getItem("tasks"))
 
-console.log(typeof arrayOftasks, arrayOftasks)
-
-
-
-for (let i of elemetsOfTaskList) {
-    arrayOftasks.push({
-        done: i.childNodes[3].childNodes[1].dataset.doneIndex,
-        text: String(i.childNodes[1].textContent),
-    })
-
-    localStorage.setItem("tasks", JSON.stringify(arrayOftasks))   
+for (let i of elemetsOfTaskList) { // Наполняем список задач если в HTML есть задачи по умолчанию, в localStorage сохраняем их
+    arrayOftasks.push({ 
+        done: i.childNodes[3].childNodes[1].dataset.doneIndex, // Статус выполнения задачи
+        text: String(i.childNodes[1].textContent), // Контент задачи
+    })   
 }
 
-function update() {
+localStorage.setItem("tasks", JSON.stringify(arrayOftasks))  // Сохраняем список задач в localStorage
+
+function update() { 
+    // Отображение списка задач
     listOfTasks.innerHTML = ''
     for (let i = 0; i < arrayOftasks.length; i++){
         listOfTasks.insertAdjacentHTML('beforeend',
@@ -38,9 +34,10 @@ function update() {
         </div>`)
     }
     
-    if (!arrayOftasks.length){
+    if (!arrayOftasks.length){ // Если нет задач, отображаем блок "нет задач"
         noTasks.style.display = "block"
-    } else {
+        
+    } else { // Скрываем блок
         noTasks.style.display = "none"
     }  
 }
@@ -48,7 +45,8 @@ function update() {
 update()
 
 function add_task () {
-    if (taskContent.value) {
+    // Если ввод не пуст, добавляем задачу в localStorage и отображаем список
+    if (taskContent.value) { 
         
         arrayOftasks.push({
             done: "0",
@@ -57,6 +55,7 @@ function add_task () {
 
         localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
         taskContent.value = ''
+        
         update()
     }
 }
@@ -65,16 +64,16 @@ addTaskButton.onclick = add_task
 
 listOfTasks.onclick = function (event) {
 
-    let index = event.target.closest(".task").dataset.index
+    let index = event.target.closest(".task").dataset.index // Получаем индекс задачи
 
-    if (event.target.className.includes("done-button")){
-        arrayOftasks[index].done =  arrayOftasks[index].done === "1" ? "0": "1"
+    if (event.target.className.includes("done-button")){ // Если нажата кнопка "задача выполнена", то меняем ее флаг и сохраняем в localStorage 
+        arrayOftasks[index].done = arrayOftasks[index].done === "1" ? "0": "1"
         localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
 
-    } else if (event.target.className.includes("delete-button")){
+    } else if (event.target.className.includes("delete-button")){ // Если нажата кнопка "удалить задачу", то удаляем ее из списка
         arrayOftasks.splice(index, 1)
 
-    } else if (event.target.className.includes("edit-button")){
+    } else if (event.target.className.includes("edit-button")){ // Если нажата кнопка "изменить задачу", то отображаем prompt и заменяем им контент
         let userInput = prompt()
         arrayOftasks[index].text = userInput
         localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
@@ -82,13 +81,13 @@ listOfTasks.onclick = function (event) {
     update()
 }
 
-document.onkeydown = function (event) {
+document.onkeydown = function (event) { // Если Enter, то добавляем задачу
     if (event.code === "Enter") {
         add_task()
     }
 }
 
-clearbutton.onclick = function () {
+clearButton.onclick = function () { // Очищаем список задач по нажатию кнопки
     arrayOftasks = []
     localStorage.setItem("tasks", "[]")
     update()
