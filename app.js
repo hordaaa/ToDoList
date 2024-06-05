0d65743ed0bc393fa53c6ee3e80d8856878f63fe
hordaaa <144600019+hordaaa@users.noreply.github.com>
2024-06-05 16:42:26 +0700

    update edit button

--- app.js
+++ app.js
@@ -8,33 +8,33 @@ let clearButton = document.getElementById("clear-button")
 if (!localStorage.getItem("tasks")) // Инициализируем список задач в localStorage
     localStorage.setItem("tasks", "[]")
 
-let arrayOftasks = JSON.parse(localStorage.getItem("tasks"))
+let arrayOfTasks = JSON.parse(localStorage.getItem("tasks"))
 
 for (let i of elemetsOfTaskList) { // Наполняем список задач если в HTML есть задачи по умолчанию, в localStorage сохраняем их
-    arrayOftasks.push({ 
+    arrayOfTasks.push({ 
         done: i.childNodes[3].childNodes[1].dataset.doneIndex, // Статус выполнения задачи
         text: String(i.childNodes[1].textContent), // Контент задачи
     })   
 }
 
-localStorage.setItem("tasks", JSON.stringify(arrayOftasks))  // Сохраняем список задач в localStorage
+localStorage.setItem("tasks", JSON.stringify(arrayOfTasks))  // Сохраняем список задач в localStorage
 
 function update() { 
     // Отображение списка задач
     listOfTasks.innerHTML = ''
-    for (let i = 0; i < arrayOftasks.length; i++){
+    for (let i = 0; i < arrayOfTasks.length; i++){
         listOfTasks.insertAdjacentHTML('beforeend',
             `<div class="task" data-index="${i}">
-            <p class="task-name ${arrayOftasks[i].done === "1" ? "done-task-text": ''}">${arrayOftasks[i].text}</p>
+            <p class="task-name ${arrayOfTasks[i].done === "1" ? "done-task-text": ''}">${arrayOfTasks[i].text}</p>
             <div class="buttons">
-                <button class="done-button ${arrayOftasks[i].done === "1" ? "done-task-button": ''}">✔</button>
+                <button class="done-button ${arrayOfTasks[i].done === "1" ? "done-task-button": ''}">✔</button>
                 <button class="delete-button">❌</button>
                 <button class="edit-button">✏️</button>
             </div>
         </div>`)
     }
     
-    if (!arrayOftasks.length){ // Если нет задач, отображаем блок "нет задач"
+    if (!arrayOfTasks.length){ // Если нет задач, отображаем блок "нет задач"
         noTasks.style.display = "block"
         
     } else { // Скрываем блок
@@ -48,12 +48,12 @@ function add_task () {
     // Если ввод не пуст, добавляем задачу в localStorage и отображаем список
     if (taskContent.value) { 
         
-        arrayOftasks.push({
+        arrayOfTasks.push({
             done: "0",
             text: String(taskContent.value),
         })
 
-        localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
+        localStorage.setItem("tasks", JSON.stringify(arrayOfTasks))
         taskContent.value = ''
         
         update()
@@ -67,16 +67,16 @@ listOfTasks.onclick = function (event) {
     let index = event.target.closest(".task").dataset.index // Получаем индекс задачи
 
     if (event.target.className.includes("done-button")){ // Если нажата кнопка "задача выполнена", то меняем ее флаг и сохраняем в localStorage 
-        arrayOftasks[index].done = arrayOftasks[index].done === "1" ? "0": "1"
-        localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
+        arrayOfTasks[index].done = arrayOfTasks[index].done === "1" ? "0": "1"
+        localStorage.setItem("tasks", JSON.stringify(arrayOfTasks))
 
     } else if (event.target.className.includes("delete-button")){ // Если нажата кнопка "удалить задачу", то удаляем ее из списка
-        arrayOftasks.splice(index, 1)
+        arrayOfTasks.splice(index, 1)
 
     } else if (event.target.className.includes("edit-button")){ // Если нажата кнопка "изменить задачу", то отображаем prompt и заменяем им контент
-        let userInput = prompt()
-        arrayOftasks[index].text = userInput
-        localStorage.setItem("tasks", JSON.stringify(arrayOftasks))
+        let userInput = prompt("Edit your task", arrayOfTasks[index].text)
+        arrayOfTasks[index].text = userInput
+        localStorage.setItem("tasks", JSON.stringify(arrayOfTasks))
     }
     update()
 }
@@ -88,7 +88,7 @@ document.onkeydown = function (event) { // Если Enter, то добавляе
 }
 
 clearButton.onclick = function () { // Очищаем список задач по нажатию кнопки
-    arrayOftasks = []
+    arrayOfTasks = []
     localStorage.setItem("tasks", "[]")
     update()
 }
